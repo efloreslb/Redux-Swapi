101938376364c5cc9049cd44787b28db68967088
Edgar F <eflores.ef@gmail.com>
2019-04-16 19:59:04 -0700

    Added actions and reducers.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
-// we'll need axios
+import axios from 'axios';
 
-// we'll need to create 3 different action types here.
-// one for fetching, one for success and one for failure
+export const PEOPLE_FETCH = "PEOPLE_FETCH";
+export const PEOPLE_SUCCESS = "PEOPLE_SUCCESS";
+export const PEOPLE_FAILURE = "PEOPLE_FAILURE";
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const getPeople = () => dispatch => {
+   dispatch({type: PEOPLE_FETCH});
+   axios.get("https://swapi.co/api/people")
+      .then(res => dispatch({type: PEOPLE_SUCCESS, payload: res.data.results}))
+      .catch(err => dispatch({type: PEOPLE_FAILURE, payload: err}));
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,34 @@
-import /* we need our action types here*/ "../actions";
+import { PEOPLE_FETCH, PEOPLE_SUCCESS, PEOPLE_FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
-  // Array characters, Boolean fetching, null error.
+  characters: [],
+  isFetching: false,
+  error: null
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case PEOPLE_FETCH:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      }
+    case PEOPLE_SUCCESS:
+      return {
+        ...state,
+        characters: action.payload,
+        isFetching: false,
+        error: null
+      }
+    case PEOPLE_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      }
     default:
       return state;
   }
